88fe807f00a162c5dabaeb960126e00f7f1f416d
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-16 07:39:40 +0430

    initialize tkinter.

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from datetime import datetime
 from timezonefinder import TimezoneFinder
 import requests
 import pytz
+
+
+root = Tk()
+root.title("Weather App")
+root.resizable(False, False)
+root.geometry("900x500+300+200")
+root.mainloop()
